44c24d09868b0b3776b4133e8e6e07aaab9777f8
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-08-25 14:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const element = document.getElementById('level');
+
+// Initialize the DOM level to 0
+const domLevel = 0;
+
+// Traverse up the DOM hierarchy while there's a parent element
+while (element.parentNode) {
+    element = element.parentNode;
+    domLevel++;
+}
+
+alert('The level of the element is: ' + domLevel);
